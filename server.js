25e6b33a8f34d077006ee131417b622ecd142b8a
Puentebravo <puentematos@gmail.com>
2021-07-15 19:36:51 -0400

    Added routing

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = require ("express")
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
+const app = express();
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
+app.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
 
-app.listen(PORT,() => 
-console.log(`Server online on: http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Server online on: http://localhost:${PORT}`)
+);
