29b7445da562c11825df161c1818b384e1626f5f
Puentebravo <puentematos@gmail.com>
2021-07-15 20:49:04 -0400

    wrote code to collect form info on button press

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,18 @@
+$(function () {
+  const answerButton = $("#aButton");
 
-$(function() {
-    
-})
+  console.log("This is working.");
+
+  answerButton.on("click", () => {
+    let colorA = $("#colorAns").val();
+    let nameA = $("#nameAns").val();
+    let sparrowA = $("#sparrowAns").val();
+    console.log("this was clicked");
+    console.log(sparrowA);
+    if (sparrowA !== "african or european") {
+      alert(`${nameA}, you have failed! *Harried flinging noises*`);
+    } else {
+      alert(`${nameA}, favoring the color of ${colorA}, you may pass.`);
+    }
+  });
+});
